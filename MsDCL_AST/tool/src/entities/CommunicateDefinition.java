5a6fb389600265647df84ec65627b7ee98f4aac4
Elder Jr <elderjr1995@gmail.com>
2018-01-09 08:57:31 -0200

    Fix HashCode CommunicateDefinition

--- MsDCL_AST/tool/src/entities/CommunicateDefinition.java
+++ MsDCL_AST/tool/src/entities/CommunicateDefinition.java
@@ -6,58 +6,63 @@ public class CommunicateDefinition {
 	private String microserviceDestin;
 	private String using;
 	private String classe;
-	
-	public CommunicateDefinition(String microserviceOrigin, String microserviceDestin, String using){
+
+	public CommunicateDefinition(String microserviceOrigin, String microserviceDestin, String using) {
 		this.microserviceOrigin = microserviceOrigin;
-		this.microserviceDestin = microserviceDestin; 
+		this.microserviceDestin = microserviceDestin;
 		this.using = using;
 	}
-	public CommunicateDefinition(String microserviceOrigin, String microserviceDestin, String using, String classe){
+
+	public CommunicateDefinition(String microserviceOrigin, String microserviceDestin, String using, String classe) {
 		this.microserviceOrigin = microserviceOrigin;
-		this.microserviceDestin = microserviceDestin; 
+		this.microserviceDestin = microserviceDestin;
 		this.using = using;
 		this.classe = classe;
 	}
-	
-	public CommunicateDefinition(String microserviceOrigin, String microserviceDestin){
+
+	public CommunicateDefinition(String microserviceOrigin, String microserviceDestin) {
 		this(microserviceOrigin, microserviceDestin, null);
 	}
-	
+
 	public String getMicroserviceOrigin() {
 		return this.microserviceOrigin;
-	} 
+	}
 
 	public String getMicroserviceDestin() {
 		return this.microserviceDestin;
 	}
-	
-	public String getUsing(){
-		return this.using; 
-	} 
+
+	public String getUsing() {
+		return this.using;
+	}
+
 	public String classe() {
 		return this.classe;
 	}
+
 	@Override
-	public int hashCode(){
-		return microserviceOrigin.length() + microserviceDestin.length() + using.length();
+	public int hashCode() {
+		if (using != null) {
+			return microserviceOrigin.length() + microserviceDestin.length() + using.length();
+		}
+		return microserviceOrigin.length() + microserviceDestin.length();
 	}
-	
+
 	@Override
-	public boolean equals(Object obj){
-		if(obj instanceof CommunicateDefinition){
+	public boolean equals(Object obj) {
+		if (obj instanceof CommunicateDefinition) {
 			CommunicateDefinition access = (CommunicateDefinition) obj;
 			return this.microserviceOrigin.equals(access.microserviceOrigin)
 					&& this.microserviceDestin.equals(access.microserviceDestin);
 		}
 		return false;
 	}
-	
+
 	@Override
-	public String toString(){
-		String s = microserviceOrigin + " communicate " + microserviceDestin; 
-		if(using != null){
-			s += " using " + this.using +"\n";
-			
+	public String toString() {
+		String s = microserviceOrigin + " communicate " + microserviceDestin;
+		if (using != null) {
+			s += " using " + this.using + "\n";
 		}
 		return s;
 	}
