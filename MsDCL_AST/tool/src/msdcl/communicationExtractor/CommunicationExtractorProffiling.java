1365ac00de55d7d486e3b90687705b5a93df8818
elenaaugusta94 <elenaaugusta94@gmail.com>
2018-01-11 14:30:06 -0200

    Atualização do extrator de comunicações

---
+++ MsDCL_AST/tool/src/msdcl/communicationExtractor/CommunicationExtractorProffiling.java
@@ -0,0 +1,17 @@
+package msdcl.communicationExtractor;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+public class CommunicationExtractorProffiling  implements MethodInterceptor{
+
+	@Override
+	public Object invoke(MethodInvocation invoke) throws Throwable {
+
+	
+	
+	}
+
+		
+	
+}
