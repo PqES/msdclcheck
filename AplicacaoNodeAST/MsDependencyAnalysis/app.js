2d35d57de9d16172ef84da6c4868047d01066522
Álvaro Martins Espíndola <alvaromaresp@hotmail.com>
2018-08-20 15:24:42 -0300

    Upgrade in DependencyAnalysis now showing lines numbers

--- AplicacaoNodeAST/MsDependencyAnalysis/app.js
+++ AplicacaoNodeAST/MsDependencyAnalysis/app.js
@@ -4,7 +4,7 @@ const esquery = require('esquery');
 const escodegen = require('escodegen');
 const fs = require('fs');
 const readline = require('readline-sync');
-
+const path = require('path');
 
 /* *
 * @param {*} element
@@ -38,11 +38,23 @@ function writeCode(code, filename) {
 }
 
 
-const file = readline.question('File path :> ');
+if(process.argv[3] === undefined)
+     var file = process.argv[2];
+else {
+     console.log(' [ERROR] File not existent!');
+     process.exit(1);
+}
 
-
+console.log (' [INFO] ANALYZING ' + file);
 const code = fs.readFileSync(file, 'utf-8').replaceAll('let\\s', 'var ');
-const ast = espree.parse(code);
+
+console.log (' [INFO] AST INITIALIZING...');
+const ast = espree.parse(code,  {
+     loc: true,
+     ecmaVersion: 6,
+     sourceType: "module"
+});
+console.log (' [INFO] AST INITIALIZED');
 
 
 var varRequireFromProject = {};
@@ -58,6 +70,8 @@ requestMsFromFunctions['global'] = [];
 requestDependencyFromFunctions['global'] = [];
 var requestMsFromExports = {};
 var requestMsFromExportsInv = {};
+var express_property =  {};
+var const_mapping = [];
 // console.log(esquery.query(ast, '[name="configFile"],[type="Identifier"]'));
 
 /**
@@ -99,15 +113,95 @@ function checkRest(node){
 
 function getNodeValue(ast, n_node, parent_node){
 
+
      if(n_node.type === 'Identifier'){
           if (identifierIsParam(n_node.name)) {
-               return '';
+               return 'IdentifierIsParam';
           }
      }
+
+
+     console.log(n_node);
+
+     if (n_node.type === 'Literal'){
+          let rAux = n_node.value;
+
+
+          if (rAux === 'env')
+               return 'IdentifierIsParam';
+
+          if (rAux !== path.basename(rAux)){
+
+               let ms_aux;
+
+               rAux = n_node.value;
+
+               let array_aux = rAux.split('/');
+
+               if (array_aux[0] === '')
+                    ms_aux = array_aux[1].toString();
+               else
+                    ms_aux = array_aux[0].toString();
+
+               let url_aux = rAux.substring(rAux.indexOf(ms_aux) + ms_aux.length, rAux.length);
+
+               rAux = {
+                    microservice : ms_aux,
+                    url : url_aux,
+                    lineStart : n_node.loc.start.line,
+                    lineEnd : n_node.loc.end.line
+               };
+          }
+
+          return rAux;
+     } else {
+
+          console.log (' [INFO] USING EQUALS CONDITIONAL');
+          let node_string = code.substring(n_node.start, n_node.end);
+
+          let array_node_string = node_string.split('\.');
+
+          // console.log(const_mapping[array_node_string[0]]);
+          console.log (array_node_string);
+
+          if (array_node_string[0].indexOf('\n') != -1)
+               array_node_string[0] = array_node_string[0].slice(0, array_node_string[0].indexOf('\n'));
+
+          if (const_mapping[array_node_string[0]] !== undefined){
+               let rNodeValue = const_mapping[array_node_string[0]].toString().replaceAll('\:', '\$');
+
+               let array_aux = rNodeValue.split('/');
+
+               let ms_aux;
+
+               if (array_aux[0] === '')
+                    ms_aux = array_aux[1].toString();
+               else
+                    ms_aux = array_aux[0].toString();
+
+               let url_aux = rNodeValue.substring(rNodeValue.indexOf(ms_aux) + ms_aux.length, rNodeValue.length);
+
+               let requestObj = {
+                    microservice : ms_aux,
+                    url : url_aux,
+                    lineStart : n_node.loc.start.line,
+                    lineEnd : n_node.loc.end.line
+               };
+
+               return requestObj;
+          }
+     }
+
+     console.log ('[INFO] USING GENERIC FUNCTION');
      let startFunction = getStartFunction();
      // let nextNode
 
-     let newAST = espree.parse(code);
+     const newAST = espree.parse(code,  {
+          loc: true,
+          ecmaVersion: 6,
+          sourceType: "module"
+     });
+
 
 
      let removing = estraverse.replace(newAST, {
@@ -133,7 +227,7 @@ function getNodeValue(ast, n_node, parent_node){
                }
                if (node.start === parent_node.start) {
                     retNode = espree.parse('module.exports.SECRET_VAR_STRING_VALUE = ' + code.substring(n_node.start, n_node.end) );
-                    console.log("-------------------------------Substring: " + code.substring(n_node.start, n_node.end));
+                    console.log(' [INFO] Substring: ' + code.substring(n_node.start, n_node.end));
                }
 
                return retNode;
@@ -145,17 +239,26 @@ function getNodeValue(ast, n_node, parent_node){
 
      if(getFunctionExportName() !== undefined)
           codeResult += '\nmodule.exports.' + getFunctionExportName() + '();\n';
-     /*else if(nodeFunction !=== undefined)
-          codeResult += '\nmodule.exports.' +  + '();\n';*/
-
-
+     /*else if(express_property !== ''){
+          console.log(express_property.argument);
+          // codeResult += '\nvar app_temp = require(\'' + __dirname + '/' + express_property.argument + '\');\n \napp.use(app_temp);\n';7
+          codeResult += '\nmodule.exports.function()\{app.' + express_property.argument + '()\};'
+     }*/
 
      let fileTemp = file.substring(0, file.length - 3) + '_temp.js';
+
      writeCode(codeResult, fileTemp);
      // console.log(codeResult);
 
 
      let rRequire = require(fileTemp).SECRET_VAR_STRING_VALUE;
+
+     fs.unlinkSync(fileTemp, (err) => {
+          if (err) throw err;});
+
+     delete require.cache[require.resolve(fileTemp)];
+
+     console.log(rRequire);
 
      if(rRequire === undefined)
           return code.substring(n_node.start, n_node.end);
@@ -245,6 +348,16 @@ estraverse.traverse(ast, {
                }
           }
 
+          if (node.type === 'CallExpression' && node.callee.type === 'MemberExpression' && node.callee.object.name === 'app'
+              && httpRequestLibrarys.contains('express') && node.arguments[0].type === 'Literal'){
+              express_property = {
+                  name: node.callee.property.name,
+                  end: node.end,
+                  argument: node.arguments[0].value.substring(1, node.arguments[0].value - 1)
+              };
+          } else if(express_property !== '' && node.start >= express_property.end){
+              express_property = '';
+          }
                // nó de chamada de função pelo objeto de uma biblioteca de requisições
           if ((node.type === 'CallExpression' &&
           ((node.callee.type === 'MemberExpression' &&
@@ -262,22 +375,16 @@ estraverse.traverse(ast, {
                else
                     parent_aux = node;
 
-               // console.log(parent);
-
                requestURL = getNodeValue(ast, node.arguments[0], parent_aux);
 
-               console.log("REQUESTED URLD: " + requestURL);
+               if (requestURL !== 'IdentifierIsParam'){
 
-               fs.unlinkSync(file.substring(0, file.length - 3) + '_temp.js', (err) => {if (err) throw err;});
+                    requestMs.push(requestURL);
 
-               delete require.cache[require.resolve(file.substring(0, file.length - 3) + '_temp.js')];
-
-               requestMs.push(requestURL);
-
-               let lastFunction = actualFunction.pop();
-               while (lastFunction !== undefined &&
-                    lastFunction.end < node.end) {
-                         lastFunction = actualFunction.pop();
+                    let lastFunction = actualFunction.pop();
+                    while (lastFunction !== undefined &&
+                         lastFunction.end < node.end) {
+                              lastFunction = actualFunction.pop();
                     }
                     if (lastFunction !== undefined) {
                          let funcionId = 'f' + lastFunction.start;
@@ -290,6 +397,33 @@ estraverse.traverse(ast, {
                          requestMsFromFunctions['global'].push(requestURL);
                     }
                }
+          } else if(node.type === 'CallExpression' && node.callee.name === 'getRoute'){
+
+               let requestURL = getNodeValue(ast, node.arguments[0], parent);
+
+               if (requestURL !== 'IdentifierIsParam'){
+
+                    requestMs.push(requestURL);
+
+                    let lastFunction = actualFunction.pop();
+                    while (lastFunction !== undefined &&
+                         lastFunction.end < node.end) {
+                              lastFunction = actualFunction.pop();
+                    }
+                    if (lastFunction !== undefined) {
+                         let funcionId = 'f' + lastFunction.start;
+                         if (requestMsFromFunctions[funcionId] === undefined) {
+                              requestMsFromFunctions[funcionId] = [];
+                         }
+                         requestMsFromFunctions[funcionId].push(requestURL);
+                         actualFunction.push(lastFunction);
+                    } else {
+                         requestMsFromFunctions['global'].push(requestURL);
+                    }
+               }
+
+          }
+
                // nó de definição de uma função
           if (node.type === 'FunctionExpression' ||
           node.type === 'ArrowFunctionExpression') {
@@ -318,37 +452,27 @@ estraverse.traverse(ast, {
                     requestMsFromExports[node.left.property.name] = node.right.name;
                }
           }
+
+          if (node.type === 'VariableDeclarator' && node.init !== null && node.init.type === 'Literal' && isNaN(node.init.value)){
+
+               let init_aux = node.init.value;
+
+               if (init_aux !== null && init_aux !== path.basename(init_aux)){
+                    if (const_mapping[node.id.name] === undefined)
+                         const_mapping[node.id.name] = [];
+
+                    const_mapping[node.id.name].push(node.init.value);
+
+                    // console.log ('CONST: ' + node.id.name + ' | Value: ' + const_mapping[node.id.name]);
+               }
+          }
      },
      leave: function(node, parent) {}
 });
 
 
-let local_file = __filename;
-local_file = local_file.substring(local_file.indexOf('api'), local_file.length - 1);
 
-let ms_name = local_file.substring(local_file.indexOf('api/'), local_file.indexOf('/index.js'));
-
-let output_file = 'communications_' + ms_name + '.txt';
-
-console.log("LOCAL FILE: " + local_file);
-
-let input_text;
-
-requestMs.forEach(function(item, index, array){
-     input_text += local_file + ' communicates ' + ms_name + ' using ' + item + '\n';
-});
-
-requestDependencyFromFunctions.forEach(function(item, index, array){
-     input_text += local_file + ' communicates ' + item.file + ' using ' + item.member + '\n';
-});
-
-requestMsFromFunctions.forEach(function(item, index, array){
-     input_text += local_file + ' communicates ' + ms_name + ' using ' + item + '\n';
-});
-
-writeCode(output_file, input_text);
-
-console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n');
+/* console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n');
 console.log("###Relatório###");
 console.log('requestMs');
 console.log(requestMs);
@@ -357,4 +481,23 @@ console.log(requestDependencyFromFunctions);
 console.log('requestMsFromFunctions');
 console.log(requestMsFromFunctions);
 console.log('requestMsFromExports');
-console.log(requestMsFromExports);
+console.log(requestMsFromExports);*/
+
+let output_file = 'communicates.txt';
+
+let output_content = '';
+
+let substring_file = file.substring(0, file.length - 3);
+
+for (let ms of requestMs){
+     console.log (substring_file + ' communicates cba-' + ms.microservice + ' using ' + ms.url + ' starting in line ' + ms.lineStart + ' and ending in line ' + ms.lineEnd);
+     output_content += substring_file + ' communicates ' + ms.microservice + ' using ' + ms.url + '\n';
+}
+
+fs.appendFile('communicates.txt', output_content , function (err) {
+  if (err) throw err;
+});
+
+/* process.stdin.setRawMode(true);
+process.stdin.resume();
+process.stdin.on('data', process.exit.bind(process, 0)); */
